b8051f13f70d42cda0e96d4fe8dafe4d5e9196e6
demna379 <demetre.maisuradze2011@gmail.com>
2025-05-24 01:48:22 +0400

    first commit

---
+++ day 30/homework/lesson000030.py
@@ -0,0 +1,3 @@
+# 4)სიაში შეინახეთ 5 ცალი სტრინგი, შემდეგ 2 იდან 4 მდე გამოიტანეთ ელემენტები
+another_random_list=["deme", "another deme", "the person who didnt get invited", "random person", "my brother"]
+print(another_random_list[2:4])
---
+++ day 30/homework/lesson0030.py
@@ -0,0 +1,2 @@
+# 2)კომენტარებით ახსენით რა არის ინდექსინგი
+# indexingi aris listis elementebis raodenoba romlis shecvlac an archevac shegvidzlia
