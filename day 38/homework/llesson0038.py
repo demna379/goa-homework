b9b539f3858cae15a0787a315358dd122c040f48
demna379 <demetre.maisuradze2011@gmail.com>
2025-06-23 23:02:32 +0400

    commit

---
+++ day 38/homework/llesson0038.py
@@ -0,0 +1,13 @@
+# 4)შექმენით ფუნქცია, რომელიც ხატავს ოთხუკთედს Turtle ბიბლიოკეთის გამოყენებით
+from turtle import *
+def forgotten_part_of_homework():
+    speed(0)
+    forward(50)
+    left(90)
+    forward(50)
+    left(90)
+    forward(50)
+    left(90)
+    forward(50)
+    exitonclick()
+forgotten_part_of_homework()
