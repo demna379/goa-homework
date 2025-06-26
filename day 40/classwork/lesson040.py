6c872c4290af09db94f9619d412a66ab0b0d01a2
demna379 <demetre.maisuradze2011@gmail.com>
2025-06-26 13:02:51 +0400

    commit

---
+++ day 40/classwork/lesson040.py
@@ -0,0 +1,8 @@
+def even_or_odd(number):
+    if number % 2 == 1:
+        return f'Odd'
+    elif number % 2 == 0:
+        return f'Even'
+    elif number == 0:
+        return f'Evan'
+even_or_odd(5)
