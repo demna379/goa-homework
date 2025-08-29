861d9d28c5b4cc331c329d0e06e2692a0c249639
demna379 <demetre.maisuradze2011@gmail.com>
2025-08-29 13:15:07 +0400

    commit

--- day 57.5/homework/6.py
+++ day 57.5/homework/6.py
@@ -1,14 +1,12 @@
 # 6)შექმენით ფუნქცი def ით, რომელსაც გადაეცემა ერთი არგუმენტი, ფუნქცია, და მეორე სია, შემდეგ ფორ ლუპით გადაუარეთ ამ სიას და თითოეულ ელემენტზე გამოიყენეთ პირველი ფუნქცია,
 # და ახალ სიაში ჩაამატეთ შემდეგ დააბრუნეთ, საბოლოოდ გამოიძახეთ ეს ფუნქცია და პირველ არგუმენტად გადაეცი lambda რომელიც გაამრავლებს რიცხვებს 5 ზე, და მეორე არგუმენტად გადაეცით რიცხვების სია
-def function_lol(func, numbers):
+def map_copy(func, numbers):
     new_list = []
     for i in numbers:
         new_list.append(func(i))
     return new_list
 
-random_list = [1,2,3,4,5]
+lstn = [1,2,3,4,5]
 
-lambda_copy = lambda x: x * 5
-
-new = function_lol(lambda_copy, random_list)
+new = map_copy(lambda x: x * 5, lstn)
 print(new)
