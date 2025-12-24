b44612a68e5bb34cdc6770e4e5504c292c2f4bac
demna379 <demetre.maisuradze2011@gmail.com>
2025-12-24 18:58:35 +0400

    commit

---
+++ day 87/classwork/script.js
@@ -0,0 +1,36 @@
+const first = document.getElementById('time1')
+const second = document.getElementById('time2')
+const third = document.getElementById("time3")
+const forth = document.getElementById('time4')
+
+const monthMaxDays = [
+  31,
+  28,
+  31,
+  30,
+  31,
+  30,
+  31,
+  31,
+  30,
+  31,
+  30,
+  31 
+];
+monthMaxDays.reverse()
+
+setInterval(function(){
+    let date = new Date()
+    let count = 0
+    for (let i of monthMaxDays.toSpliced(12-date.getMonth())){
+        count += i
+    }
+    let days = count-date.getDate()
+    let seconds = 59-date.getSeconds()
+    let minutes = 59-date.getMinutes()
+    let hourse = 23-date.getHours()
+    first.textContent = days
+    second.textContent = hourse
+    third.textContent = minutes
+    forth.textContent = seconds
+},1)
