fe9c00e52752388012e59b937a246c6a78e0b9f9
demna379 <demetre.maisuradze2011@gmail.com>
2025-12-28 15:35:57 +0400

    commit

---
+++ day 93/classwork/script.js
@@ -0,0 +1,15 @@
+// created a variable named MyAge which stores our age
+const MyAge = 8
+// created a variable named earlyYears which stores temporary value
+let earlyYears = 2
+earlyYears = 10.5
+let LaterYears = MyAge - 2
+LaterYears= 4
+console.log(`${earlyYears} ${LaterYears}`)
+// creates a variable called myAgeInDogYears which stores two added numbers: earlyYears + LaterYears
+myAgeInDogYears = earlyYears + LaterYears
+// creates a variable called myName which stores a value being converted to lower case
+let myName = 'Demetre'.toLowerCase()
+// consoles my name, my age and my age in dog years
+console.log(`My name is ${myName}. I am ${MyAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.
+`)
