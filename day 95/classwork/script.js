19abba20f5f200647f0370a4bc430c59ac395222
demna379 <demetre.maisuradze2011@gmail.com>
2026-01-17 13:13:51 +0400

    commit

---
+++ day 95/classwork/script.js
@@ -0,0 +1,25 @@
+// 1)დაწერეთ რა განსხვავებაა var სა და let ს შორის
+
+// 2)ახსენით თქვენი  სიტყვებით რა არის block scope, global scope, function scope, და მოიყვანეთ თითო მაგალითი
+
+// var-is block scopeshi sheqmnisas miwdoma mainc sheidzleba (very very very bad) da letit ara (global scopidan miwvdoma)
+
+// block scope aris figuruli frchxilebis shignit dawerili kodi romlis miwvdoma global scopidan ar sheidzleba (mxolod shignidan)
+// global scope aris tviton gareta kodi romlis miwvdoma sheidzleba nebismieri gzit
+// function scope aris functiashi dawerili kodi romlis miwvdoma
+
+
+// examples
+// global scope
+// function nig(){
+// }
+
+// function scope
+// () => {
+//    let uuh =  '' <- function scope 
+// }
+
+// block scope
+// {
+    // let demnas_iq = -50
+// }
---
+++ day 95/homework/script.js
@@ -0,0 +1,30 @@
+// 1)  ახსენით რა არის global scope
+
+// 2)  ახსენით რა არის function scope
+
+// 3)  ახსენით რა არის block scope
+
+// 4)  შექმენით სია 10 ელემენტით და მისი სრული დესტრუქცია გააკეთეთ
+
+// 5)  შექმენით ობიექტი 5 კუთვნილებით და მისი სრული დესტრუქცია მოახდინეთ
+
+// global scope aris ase vtqvat sivrce romelshic kodi iwereba. am kodis migweva nebismierad sheidzleba
+
+//  function scope aris funqciis shignit sheqmnili kodi. magas garedan ver miwvdebi, mxolod shignidan
+
+// block scope aris rame frchxilshi dawerili kodi. magas ver miwvdebi tu var keywordi ar gamoiyene (never use it 🥹🥀)
+
+let lstn = [1,2,3,4,5,6,7,8,9,10]
+
+let [one, ...rest] = lstn
+console.log(rest)
+
+let breed = {
+    color: 'red',
+    name: 'dagli',
+    logic: 'none',
+    awarenes: 'zero',
+    mySenseOfHumor: 'negative'
+} 
+
+let {color, name, logic, awarenes, mySenseOfHumor} = breed
