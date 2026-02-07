fbcd8e4d64ac6b31e4809d1a200f035646854293
demna379 <demetre.maisuradze2011@gmail.com>
2026-02-07 13:23:20 +0400

    commit

--- day 101/homework/script.js
+++ day 101/homework/script.js
@@ -2,23 +2,23 @@
 //  a ზე და მთავრდება a ზე (მაგალითად ana, anamaria, axtona)
 let names = ['ana','anamaria','axtona','demna']
 let names2 = names.filter((el) => {
-    return el[0] != 'a' && el[el.length] != 'a'
+    return el[0] == 'a' && el[el.length] == 'a'
 })
 
 // 2)შექმენით სახელების და რიცხვების სია, შემდეგ გაფილტრეთ ეს სია ისე რომ დარჩეს მარტო კენტ ინდექსზე მდგომი სტრინგები და კენტი რიცხვები
 let nums = [1,2,3,'ana','ragaca']
 let nums2 = nums.filter((el,ind) => {
-    return typeof el == 'number' ? el % 2 == 1 : typeof el == 'string' && ind%2==1?el : 0
+    return (el % 2 != 0 && typeof el == 'string') || (el % 2 != 0)
 })
 
 // 3)შექმენით რიცხვების სია, შემდეგ მარტო ლუწ ინდექსზე მდგომი ელემენტები დატოვეთ ფილტრაციით
 let numb = [5,6,7,8]
-let numb2 = numb.filter((el,ind) => ind % 2 == 0 ? el : 0)
+let numb2 = numb.filter((el,ind) => ind % 2 == 0)
 
 // 4)მოიძიეთ არის თუარა იგივე მეთოდები javascript ში (filter, map), რა განსხვავებაა პითონისა და js ის ამ ორ მეთოდს შორის
 // python
-// map(variable,func)
-// filter(variable,func)
+// map(func,variable)
+// filter(func,variable)
 
 // javascript
 // variable.map(func)
