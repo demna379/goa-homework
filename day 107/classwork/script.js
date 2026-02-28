bfaca9b9204bdb25c7757d8e463be1b6130b0814
demna379 <demetre.maisuradze2011@gmail.com>
2026-02-28 13:02:28 +0400

    commit

---
+++ day 107/classwork/script.js
@@ -0,0 +1,14 @@
+// 1) ახსენით რა არის dom ი დეტალურად, თუ სახატავ ხელსაწყოს გამოიყენებთ უკეთესი
+// და როცო მუშაობს dom
+
+// DOM ari Document Object Model romelic aris saitis mtliani structura (branch)
+// DOM shedgeba parent elementebisgan da child elementebisgna pirveli aris HMTL, mere body, da mere bodys shignit sheqmnili tagebi
+// rogor mushaobs DOM: DOM aris mtliani saitis structura romelsac javascriptic shegvidzlia mivwvdet. magalitad raime elements. (body,h1,p)
+
+
+// visual
+//      DOM
+          
+//      HTML
+//      body
+//      child elements
