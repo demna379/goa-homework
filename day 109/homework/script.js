69c6d95a35514e69e666f6ab5a5a26ce8aac41dd
demna379 <demetre.maisuradze2011@gmail.com>
2026-03-06 22:58:40 +0400

    commit

---
+++ day 109/homework/script.js
@@ -0,0 +1,18 @@
+// 3) დაწერე პროგრამა, რომელიც მომხმარებელს სთხოვს შეიყვანოს ნებისმიერი მთელი რიცხვი n. პროგრამამ for ციკლის გამოყენებით უნდა გამოთვალოს ყველა რიცხვის ჯამი 1-დან n-მდე.
+// let username = Number(prompt('Enter any non-negative number: '))
+// let count = 0
+// for (let i = 0; i < Number; i++){
+//     count += i
+// }
+// 4) შექმენი ცვლადი password = "1234". სთხოვე მომხმარებელს შეიყვანოს პაროლი მანამ, სანამ ის სწორად არ გამოიცნობს მას. თუ შეყვანილი ტექსტი არასწორია, პროგრამამ თავიდან უნდა სთხოვოს შეყვანა.
+// let password = '1234'
+// let username1 = prompt('guess four digit password: ')
+// while (username1 != password){
+//     username1 = prompt('guess four digit password: ')
+// }
+// 5) გამოიტანე კონსოლში ყველა ლუწი რიცხვი 1-დან 50-მდე for ციკლის გამოყენებით.
+// for (let index = 0; index < 50; index++) {
+//     if (index % 2 == 0){
+//         console.log(index)
+//     }
+// }
