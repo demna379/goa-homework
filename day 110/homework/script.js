5397006be59492061298df69dce139e36918e3c3
demna379 <demetre.maisuradze2011@gmail.com>
2026-03-11 18:38:36 +0400

    commit

---
+++ day 110/homework/script.js
@@ -0,0 +1,25 @@
+// 1)ახსენით რა განსხვავება / მსგავსებაა for loop სა და while loop ს შორის
+// msgavseba is aris rom orive ragac kods aketebs iqamde sanam ragac condition false iqneba
+// gansxvaveba is aris rom for loops vuwers ramdenjer unda gavushvat kodi da while loops veubnebit rodis gacherdes
+
+// 2)დარეგისტრირდით codecademy ზე
+// nuh uh
+
+// 3)გამოიტანე  რიცხვები 50 დან 70 მდე for loop ით
+for (let index = 50; index < 70; index++) {
+    console.log(index)
+}
+
+// 4)გამოიტანე  რიცხვები 50 დან 150 მდე while loop ით
+count = 50
+while (count < 150) {
+    console.log(count)
+    count ++
+}
+
+// 5)გამოიტანე 1 დან 50 მდე მხოლოდ ლუწი რიცხვები
+for (let index = 0; index < 50; index++) {
+    if (index % 2 == 0){
+        console.log(index)
+    }    
+}
