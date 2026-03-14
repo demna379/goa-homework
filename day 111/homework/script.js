6cf7447fd0a4ceffb7d3b6c22899a834256151d4
demna379 <demetre.maisuradze2011@gmail.com>
2026-03-14 13:20:02 +0400

    commit

---
+++ day 111/homework/script.js
@@ -0,0 +1,11 @@
+// 1) ახსენით რა არის object, მოიყვანეთ რეალური მაგალითები
+// object aris sia, masivi romelic sheicavs tvisebebs, properties
+
+// 2) მოიყვანეთ რამოდენიმე ჩაშენებული ჯავასკრიპტის ობიექტი და მათი მეთოდები/მახასიათებლები
+// Number, String, null, Object, undefined
+
+// 3) ახსენით რა არის callback, მოიყვანეთ მაგალითი
+// funqcias argumentad gadaecema funqcia
+
+// 4) ჩამოწერეთ ფუნქციის შექმნის გზები და მოიყვანეთ მაგალითები
+// default, function expression, arrow function
