88be1aea0b107fa2b43bf643a8a9a15cb707458c
demna379 <demetre.maisuradze2011@gmail.com>
2026-03-28 14:53:25 +0400

    commit

---
+++ day 116/classwork/script.js
@@ -0,0 +1,35 @@
+ const obj = {
+  arr: [
+    // item1
+    {
+      level: [10, 50, 100],
+    },
+
+    // item2
+    {
+      level: [20, 40, 60],
+      students: [
+        {
+          name: "jemala",
+          surname: "barkalaia",
+          score: [8, 8, 10],
+        },
+        {
+          name: "jumbera",
+          surname: "lamazi",
+          score: [10, 7, 9],
+        },
+      ],
+    },
+
+  ],
+};
+
+const {arr} = obj
+
+const [item1, item2] = arr
+
+const {level:level1} = item1
+const [one,two,three] = level1
+
+const {level:level2} = item2
