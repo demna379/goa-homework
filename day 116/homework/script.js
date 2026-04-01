b4b7945ac15bdc462e2486ad1feeae1f57d7f02c
demna379 <demetre.maisuradze2011@gmail.com>
2026-04-01 18:39:36 +0400

    commit

---
+++ day 116/homework/script.js
@@ -0,0 +1,53 @@
+ const obj = {
+  arr: [
+    // item1
+    {
+      level: [10, 50, 100],
+    },
+
+    // item2
+    {
+      level: [20, 40, 60],
+      students: [
+        {
+          name: "jemala",
+          surname: "barkalaia",
+          score: [8, 8, 10],
+        },
+        {
+          name: "jumbera",
+          surname: "lamazi",
+          score: [10, 7, 9],
+        },
+      ],
+    },
+
+  ],
+};
+
+ const {
+  arr: [
+    // item1
+    {
+      level: [one,two,three],
+    },
+
+    // item2
+    {
+      level: [four,five,six],
+      students: [
+        {
+          name: nigger,
+          surname: absolute_nigger,
+          score: [nigger1,nigger2,nigger3],
+        },
+        {
+          name: whotfinfitedthisguy,
+          surname: HEILHITLER,
+          score: [absolute_nigger1,absolute_nigger2,absolute_nigger3],
+        },
+      ],
+    },
+
+  ],
+} = obj
