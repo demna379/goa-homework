7802c5577387896cd180cc52c23c3b34023e3973
demna379 <demetre.maisuradze2011@gmail.com>
2026-05-23 13:02:57 +0400

    commit

---
+++ day 131/homework/script.js
@@ -0,0 +1,10 @@
+// მოიძიეთ ინფორმაცია რეაქთზე და უყურეთ დღევანდელ ჩანაწერს ახლიდან, დეტალურად ახსენით რა არის node,
+//  რისთვის გამოიყენება და რა დანიშნულება აქვს vite ს, რა არის npm და რის საშუალებას გვაძლევს,
+//  რა უპირატესობები აქვს react ა სხვა framework ებთან შედარებით
+
+// node aris environment romelic ushvebs javascriptis kods browseris gareshe da mxolo DOM-tan ar aq wvdoma
+// react aris framework romelic gvexmareba saitis gaketebashi
+// vite-s sashualebit chven shegvidzlia gavushvat chveni saiti
+// npm nishnavs Node Package Manager da ubralod romelic inaxavs frameworkebs da magis sashualebit viwert
+
+// ასევე დაასრულეთ წინა დავალება ვისაც არ დაგისრულებიათ(დაჰოსთეთ weather app)
