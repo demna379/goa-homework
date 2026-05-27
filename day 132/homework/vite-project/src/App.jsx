e2e49ca59df11135acb1be27172f9e0f58f9bfcd
demna379 <demetre.maisuradze2011@gmail.com>
2026-05-27 19:06:13 +0400

    commit

---
+++ day 132/homework/vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <>
+      <div id="parent">
+        <div id="first">
+
+        </div>
+
+        <div id="second">
+          <p id="perfume">P E R F U M E</p>
+          <h1 id="random-text">Gabrielle <br /> Essence Eau <br /> De Parfum</h1>
+          <p id="another">
+            A floral, solar and voluptuous interpretation composed by Olivier Polge,
+            Perfumer-Creator for the House of CHANEL.
+          </p>
+          <div id="prices">
+            <h1 id="price">$149.99</h1> <p id="underlined">$169.99</p>
+          </div>
+          <button id="button"><img src="./icon-cart.svg" alt="" /> Add to Cart</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
