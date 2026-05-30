660f51bc60d3852ec3bb4da5e38240e5f2c4f56b
demna379 <demetre.maisuradze2011@gmail.com>
2026-05-30 12:58:25 +0400

    commit

---
+++ day 133/classwork/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col gap-[20px] items-center">
+        <img src="./image.png" alt="" />
+        <h1 className="font-[700] text-[20px] text-center">Improve your front-end <br />skills by building projects</h1>
+        <p className="text-[grey] text-center">Scan the QR code to visit Frontend <br />Mentor and take your coding skills to <br />the next level</p>
+      </div>
+    </>
+  )
+}
+
+export default App
