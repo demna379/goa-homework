d65c4c79f0d85e1bf4c5824c6aecdbe4992365a3
demna379 <demetre.maisuradze2011@gmail.com>
2026-06-26 12:18:16 +0400

    commit

--- day 139/homework/random/src/App.jsx
+++ day 139/homework/random/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <>
-      <div className="flex min-w-[250px] w-[60%] m-[20px] flex-wrap text-[white]">
-        <div className="h-[400px] min-w-[250px] w-[33%] rounded-[10px_0_0_10px] bg-[#e28525] p-[40px] flex flex-col gap-[10px]">
+      <div className="flex min-w-[250px] w-[60%] m-[20px] max-[755px]: flex-col text-[white] items-center justify-center">
+        <div className="h-[400px] min-w-[250px] w-[33%] rounded-[10px_0_0_10px] bg-[#e28525] p-[40px] flex flex-col gap-[10px] max-[755px]:rounded-[10px_10px_0_0]">
           <img className="w-[80px]" src="./icon-sedans.svg" alt="" />
           <h1 className="text-[30px] font-[700]">SEDANS</h1>
           <p className="m-[0_0_30px_0]">
@@ -28,7 +28,7 @@ function App() {
           <button className="bg-[white] text-[#016972] w-[140px] rounded-[40px] h-[40px]">Learn More</button>
         </div>
 
-        <div className="h-[400px] min-w-[250px] w-[33%] rounded-[0_10px_10px_0] bg-[#00403f] p-[40px] flex flex-col gap-[10px]">
+        <div className="h-[400px] min-w-[250px] w-[33%] rounded-[0_10px_10px_0] bg-[#00403f] p-[40px] flex flex-col gap-[10px] max-[755px]:rounded-[0_0_10px_10px]">
           <img className="w-[80px]" src="./icon-luxury.svg" alt="" />
           <h1 className="text-[30px] font-[700]">luxury</h1>
           <p className="m-[0_0_30px_0]">
